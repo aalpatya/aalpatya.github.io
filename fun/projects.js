8466ab1a42e72532ababe0f2218bbc61ac956af3
Aalok Patwardhan <aalok@aalok.com>
2026-05-28 16:10:51 +0100

    fun projects added

---
+++ fun/projects.js
@@ -0,0 +1,9 @@
+window.funProjects = [
+  {
+    title: "Assassin",
+    slug: "assassin",
+    url: "../assassin/index.html",
+    mark: "🥷",
+    description: "Secret targets, odd objects, suspicious places."
+  }
+];
