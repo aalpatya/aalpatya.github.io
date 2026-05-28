ea3454c37841fa72a2bbb1977a0e1d4b4a5c6478
Aalok Patwardhan <aalok@aalok.com>
2026-05-28 21:57:00 +0100

    add chameleon game

--- fun/projects.js
+++ fun/projects.js
@@ -5,5 +5,12 @@ window.funProjects = [
     url: "../assassin/index.html",
     mark: "🥷",
     description: "Secret targets, odd objects, suspicious places."
+  },
+  {
+    title: "Chameleon",
+    slug: "chameleon",
+    url: "../chameleon/index.html",
+    mark: "🦎",
+    description: "One secret word, one hidden bluff."
   }
 ];
